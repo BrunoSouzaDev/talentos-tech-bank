67d186345406f6368e04b127fe24ab47581cb88b
Bruno Souza <bruno1994deilson@gmail.com>
2021-06-29 07:54:03 -0300

    First commit

---
+++ src/dio0/main.java
@@ -0,0 +1,10 @@
+package dio0;
+
+public class main {
+
+    public static void Main(String[] args) {
+        System.out.println("Olá talentos tech bank");
+        System.out.println(System.getProperty("java.version"));
+    }
+
+}
